d82138f050b8169e43e8ca3825668c12304f6013
akbar-ardiansyah <73751746+akbar-ardiansyah@users.noreply.github.com>
2022-07-30 22:59:42 +0700

    mereturn halaman home

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+import Main from './container/main/main';
 import reportWebVitals from './reportWebVitals';
-import HelloComponent from './component/HelloComponent';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelloComponent />
+    <Main />
   </React.StrictMode>
 );
-
-
 
 
 // If you want to start measuring performance in your app, pass a function
