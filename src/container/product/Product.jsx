f5fde1415c6d88991fd53279d85a058c937b9271
akbar-ardiansyah <73751746+akbar-ardiansyah@users.noreply.github.com>
2023-01-01 23:06:50 +0700

    change style on product.jsx

--- src/container/product/Product.jsx
+++ src/container/product/Product.jsx
@@ -1,6 +1,7 @@
 import React,{Component, Fragment} from "react";
 
 import CardContent from "../../component/card/CardContent";
+import Carousel from "../../component/carousel/carousel";
 import './Product.css';
 
 class Product extends Component{
@@ -19,7 +20,12 @@ class Product extends Component{
                         </div>
                     </div>
                 </div>
-                <section className="content">
+                <section className="container">
+                <Carousel 
+                    carouselImages="https://static.vecteezy.com/packs/media/vectors/term-bg-1-666de2d9.jpg"
+                    carouselImage1="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                    carouselImage2="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvKH4iA4wP-Q6VKAAQm8SOrTAVQmvp70g0clZbu3uPuEjh_Fh7cdWzhamCD6zd8l8EDI8&usqp=CAU"
+                    />
                 <CardContent 
                 title="title 1" 
                 dataImg="https://static.vecteezy.com/packs/media/vectors/term-bg-1-666de2d9.jpg" 
@@ -38,7 +44,6 @@ class Product extends Component{
                 desc="deserunt voluptatibus beatae voluptas distinctio, quo nisi eius maiores fugit dignissimos quas recusandae. Consectetur!"
                 price="400"
                 />
-
                 </section>
             </Fragment>
         );
