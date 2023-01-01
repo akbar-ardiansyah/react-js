30e84138c787311dd690df16356f7aa77083ccdf
akbar-ardiansyah <73751746+akbar-ardiansyah@users.noreply.github.com>
2023-01-01 23:05:48 +0700

    turnoff reportWebVitals();

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import Main from './container/main/main';
 import reportWebVitals from './reportWebVitals';
 
 
+// disini adalah tempat dimana koomponen yang telah kita buat di render
+// ada dua macam komponen yaitu statefullComponent dan statlessComponent
+// berikut adalah contoh koponen default yang telah di buat sejak pertama kali di create projects
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -16,4 +20,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
