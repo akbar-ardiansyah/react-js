f501734f35f7b73ff096df271098abeb94989771
akbar-ardiansyah <73751746+akbar-ardiansyah@users.noreply.github.com>
2023-01-05 22:01:55 +0700

    mengirim newValue ke halaman Product.jsx

--- src/container/counter/counter.jsx
+++ src/container/counter/counter.jsx
@@ -8,7 +8,11 @@ class counter extends Component {
         // handleCounterChange=(newValue)=>{
         //     this.props.onCounterChange(newValue);
         // }
-
+        // mengirim newValue ke halaman Product.jsx
+        addchart=(newValue)=>{
+            // this.props.addToCart(newValue)
+            this.props.orders + 1
+        }
         handleClickMax = () => {
             this.setState({
                 order: this.state.order + 1
@@ -24,7 +28,7 @@ class counter extends Component {
     render() {
         return (
         <div className="button">
-                <button>add to cart</button>
+                <button onClick={this.addchart}>add to cart</button>
                 <div className="form">
                 <button className="min" onClick={this.handleClickMin} >-</button>
                 <input type="text" readOnly value={this.state.order}/>
