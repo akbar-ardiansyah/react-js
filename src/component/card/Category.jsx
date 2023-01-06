ccd3b9e167569f87c578e01bff24b0790057145c
akbar-ardiansyah <73751746+akbar-ardiansyah@users.noreply.github.com>
2023-01-06 17:28:40 +0700

    menambah html scrollbar

---
+++ src/component/card/Category.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Category.css";
+
+const Category = (props)=>{
+        return(
+            <div className="container p-2 m-1">
+                <div className="scroll-x grid text-center">
+                    <div className="item-category">
+                        <i className="bi bi-alarm"></i>Lorem, ipsum.
+                    </div>
+                    <div className="item-category">
+                        <i className="bi bi-alarm"></i>Lorem, ipsum dolor.
+                    </div>
+                    <div className="item-category">
+                        <i className="bi bi-alarm"></i>Lorem ipsum dolor sit amet.
+                    </div>
+                    <div className="item-category">
+                        <i className="bi bi-alarm"></i>Lorem, ipsum.
+                    </div>
+                    <div className="item-category">
+                        <i className="bi bi-alarm"></i>Lorem ipsum dolor sit amet consectetur.
+                    </div>
+                    <div className="item-category">
+                        <i className="bi bi-alarm"></i>Lorem ipsum dolor sit
+                    </div>
+                    <div className="item-category">
+                        <i className="bi bi-alarm"></i>Lorem ipsum dolor sit
+                    </div>
+                    <div className="item-category">
+                        <i className="bi bi-alarm"></i>Lorem ipsum dolor sit
+                    </div>
+                    <div className="item-category">
+                        <i className="bi bi-alarm"></i>Lorem ipsum dolor sit
+                    </div>
+                </div>
+            </div>
+        );
+}
+
+export default Category;
