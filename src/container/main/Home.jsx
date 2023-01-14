3712ace67bf5201f280586389095667c5cd67f72
akbar-ardiansyah <73751746+akbar-ardiansyah@users.noreply.github.com>
2023-01-14 21:02:11 +0700

    merreturn elemen post

--- src/container/main/Home.jsx
+++ src/container/main/Home.jsx
@@ -1,13 +1,13 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import LifeCycleComp from "../lifecycle/LifeCycleComp";
 import Post from "../post/Post"
 
-class Home extends Component{
-    state={
+class Home extends Component {
+    state = {
         showComponent: true
     }
     // componentDidMount pencopotan component
-    componentDidMount(){ 
+    componentDidMount() {
         // setTimeout(()=>{
         //     this.setState({
         //      showComponent: false
@@ -15,8 +15,8 @@ class Home extends Component{
         //     // window.location.href="https://google.com";
         // }, 15000)
     }
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <div className="container">
                     {/* <div className="position-absolute top-50 start-50 translate-middle">
@@ -28,12 +28,12 @@ class Home extends Component{
                             : null
                         }
                     </div>    */}
-                    <Post/>
+                    <Post />
                 </div>
             </div>
         )
     }
-    
+
 }
 
 export default Home;
