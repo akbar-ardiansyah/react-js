194ef2749254b9a8cf06a48f3a66696a81f7f49e
akbar-ardiansyah <73751746+akbar-ardiansyah@users.noreply.github.com>
2023-01-14 21:01:40 +0700

    iplement api menggunakan axios

--- src/container/post/Post.jsx
+++ src/container/post/Post.jsx
@@ -1,32 +1,42 @@
-import React,{Component,Fragment} from "react";
+import React, { Component, Fragment } from "react";
 import BlockPost from "../../component/blockpost/Blockpost";
 import "./Post.css";
+import axios from "axios";
+
 
 class Post extends Component {
-    state ={
+    state = {
         // looping data dari jason
-        blockPost:[]
+        blockPost: []
     }
-    componentDidMount(){
-        // parse data dari API dan mengembalikan JSON ke dalam state BlockPost 
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(json => {
-            this.setState({
-                blockPost : json
+    componentDidMount() {
+        // parse dengan metode fetch, data dari API dan mengembalikan JSON ke dalam state BlockPost 
+        // fetch('https://jsonplaceholder.typicode.com/posts')
+        //     .then(response => response.json())
+        //     .then(json => {
+        //         this.setState({
+        //             blockPost: json
+        //         })
+        //     })
+        // parse mengunakan AXIOS sama halnya dengan contho di atas
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then((result) => {
+                this.setState({
+                    blockPost: result.data
+                })
             })
-        })
+        // kelebihan axios memiliki   kelebihan  yaitu dia menyimpat token di headers
     }
-    render(){
-        return(
+    render() {
+        return (
             <Fragment>
                 {
                     // melakukan maping agar data mudah di panggil
                     this.state.blockPost.map(blockpost => {
-                        return(
+                        return (
                             // cotoh pemanggilan data berbentuk object pada map
                             <a href="#" className="blockpost-anchor">
-                            <BlockPost key={blockpost.id} title={blockpost.title} excerpt={blockpost.body} authors="Rahmad" /> 
+                                <BlockPost key={blockpost.id} title={blockpost.title} excerpt={blockpost.body.length < 80 ? blockpost.body.substring(0, 200) : blockpost.body.substring(0, 200) + " ..."} authors="Rahmad" />
                             </a>
                         )
                     })
