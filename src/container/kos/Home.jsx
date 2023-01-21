80867b2ccf7e59b8171668d07eb1cda63b81bf03
akbar-ardiansyah <73751746+akbar-ardiansyah@users.noreply.github.com>
2023-01-21 23:59:01 +0700

    stateless component

---
+++ src/container/kos/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from "react";
+import Main from '../../page/Main'
+import './Home.css'
+class Home extends Component {
+
+    render() {
+        return (
+            <Main />
+        )
+    }
+}
+export default Home;
