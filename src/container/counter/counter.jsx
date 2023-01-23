ed0b55835f11c494c262440dd5612ad473d21898
akbar-ardiansyah <73751746+akbar-ardiansyah@users.noreply.github.com>
2023-01-23 17:50:44 +0700

    merapikan counter.jsx

--- src/container/counter/counter.jsx
+++ src/container/counter/counter.jsx
@@ -1,40 +1,40 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 // import '../../component/card/CardContent.css';
 
 class counter extends Component {
     state = {
-        order:0
-        }
-        // handleCounterChange=(newValue)=>{
-        //     this.props.onCounterChange(newValue);
-        // }
-        // mengirim newValue ke halaman Product.jsx
-        addchart=(newValue)=>{
-            // this.props.addToCart(newValue)
-            this.props.orders + 1
-        }
-        handleClickMax = () => {
+        order: 0
+    }
+    // handleCounterChange=(newValue)=>{
+    //     this.props.onCounterChange(newValue);
+    // }
+    // mengirim newValue ke halaman Product.jsx
+    // addchart = (newValue) => {
+    //     // this.props.addToCart(newValue)
+    //     this.props.orders + 1
+    // }
+    handleClickMax = () => {
+        this.setState({
+            order: this.state.order + 1
+        })
+    }
+    handleClickMin = () => {
+        if (this.state.order > 0) {
             this.setState({
-                order: this.state.order + 1
+                order: this.state.order - 1
             })
         }
-        handleClickMin = () => {
-            if(this.state.order > 0){
-                this.setState({
-                    order: this.state.order - 1
-                })
-            }
-        }
+    }
     render() {
         return (
-        <div className="button">
+            <div className="button">
                 <button onClick={this.addchart}>add to cart</button>
                 <div className="form">
-                <button className="min" onClick={this.handleClickMin} >-</button>
-                <input type="text" readOnly value={this.state.order}/>
-                <button className="max" onClick={this.handleClickMax}>+</button>
+                    <button className="min" onClick={this.handleClickMin} >-</button>
+                    <input type="text" readOnly value={this.state.order} />
+                    <button className="max" onClick={this.handleClickMax}>+</button>
                 </div>
-        </div>
+            </div>
         );
     }
 }
