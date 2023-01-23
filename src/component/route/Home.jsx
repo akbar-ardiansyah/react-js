cd24f2bb47f81d45d57d3c52e4de4da2d4914a87
akbar-ardiansyah <73751746+akbar-ardiansyah@users.noreply.github.com>
2023-01-23 17:49:35 +0700

    menambah komponent home

---
+++ src/component/route/Home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="card">
+                <div className="card-title">
+                    Home
+                </div>
+                <div className="card-body">
+                    <h1>ini adalah halaman Home</h1>
+                </div>
+            </div>
+        )
+    }
+}
+export default Home;
