559c6080b1d3d005eb836933c710dab9551ec015
akbar-ardiansyah <73751746+akbar-ardiansyah@users.noreply.github.com>
2023-01-23 17:51:07 +0700

    merapikan  LifeCycleComp

--- src/container/lifecycle/LifeCycleComp.jsx
+++ src/container/lifecycle/LifeCycleComp.jsx
@@ -1,21 +1,21 @@
-import react,{Component} from 'react';
+import React, { Component } from 'react';
 import './LifeCycleComp.css';
 
 
 class LifeCycleComp extends Component {
-    constructor(props){ //constructor digunakan untuk menginisialisasi state
+    constructor(props) { //constructor digunakan untuk menginisialisasi state
         super(props); //karena LifeCycleComp extend dari Component maka harus mendifine super(props)
-        this.state={
-            count: 1
+        this.state = {
+            count: 0
         }
         console.log('constructor');
         // this.handleCounterChange=this.handleCounterChange.bind(this);
     }
-    static getDerivedStateFromProps(props,state){ //getDerivedStateFromProps digunakan untuk mengupdate state
-    console.log('getDerivedStateFromProps');
+    static getDerivedStateFromProps(props, state) { //getDerivedStateFromProps digunakan untuk mengupdate state
+        console.log('getDerivedStateFromProps');
         return null;
     }
-    componentDidMount(){ //componentDidMount digunakan untuk mengakses DOM
+    componentDidMount() { //componentDidMount digunakan untuk mengakses DOM
         console.log('componentDidMount');
         // setTimeout(() => {
         //     this.setState({
@@ -23,14 +23,14 @@ class LifeCycleComp extends Component {
         //     });
         // }, 5000);
     }
-    shouldComponentUpdate(nextProps,nextState){ //shouldComponentUpdate digunakan untuk mengecek apakah state atau props berubah
+    shouldComponentUpdate(nextProps, nextState) { //shouldComponentUpdate digunakan untuk mengecek apakah state atau props berubah
         // shouldComponentUpdate dapat melakukan pada nextstate
         console.group('shouldComponentUpdate');
         // console.log("nextProps: ",nextProps)
-        console.log("nextState: ",nextState)
+        console.log("nextState: ", nextState)
         console.groupEnd();
 
-        if(nextState.count >= 4){
+        if (nextState.count >= 4) {
             // document.writeln('so many click this button')
             // this.setState({
             //     count:"so many click this button"
@@ -38,35 +38,34 @@ class LifeCycleComp extends Component {
             return false
         }
         return true
-        
+
     }
-    getSnapshotBeforeUpdate(prevProps,prevState){ //getSnapshotBeforeUpdate digunakan untuk mengambil data sebelum update
+    getSnapshotBeforeUpdate(prevProps, prevState) { //getSnapshotBeforeUpdate digunakan untuk mengambil data sebelum update
         console.log('getSnapshotBeforeUpdate');
         return null;
     }
-    componentDidUpdate(prevProps,prevState, snapshot){ //componentDidUpdate
+    componentDidUpdate(prevProps, prevState, snapshot) { //componentDidUpdate
         console.log('componentDidUpdate');
     }
-    componentWillUnmount(){ //componentWillUnmount
+    componentWillUnmount() { //componentWillUnmount
         console.log('componentWillUnmount');
     }
 
-    counter = ()=>{
+    counter = () => {
         this.setState({
-            count: this.state.count +1
+            count: this.state.count + 1
         })
     }
-    render(){
-        console.log('render');
-    return(
-        <div>
-            <h1 className='text-center'>
-                {this.state.count}
-            </h1>
-            <button className='btn-dark' onClick={this.counter}>this.button</button>
-        </div>
-    );
-}
+    render() {
+        return (
+            <div>
+                <h1 className='text-center'>
+                    {this.state.count}
+                </h1>
+                <button className='btn-dark' onClick={this.counter}>this.button</button>
+            </div>
+        );
+    }
 }
 
 export default LifeCycleComp;
