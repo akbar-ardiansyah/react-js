402d8a6bed03e5f6a007bfc31db9a2cb030e779c
akbar-ardiansyah <73751746+akbar-ardiansyah@users.noreply.github.com>
2023-01-23 17:49:23 +0700

    menambah component about

---
+++ src/component/route/About.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class About extends Component {
+    render() {
+        return (
+            <div className="card">
+                <div className="card-title">
+                    About
+                </div>
+                <div className="card-body">
+                    ini adalah halaman About ha jen
+                </div>
+            </div>
+        )
+    }
+}
+export default About;
