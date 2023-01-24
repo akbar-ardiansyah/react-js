dafc6f8e6cc521a80c2bc81e5aad0ff2ddd02822
akbar-ardiansyah <73751746+akbar-ardiansyah@users.noreply.github.com>
2023-01-24 21:25:05 +0700

    fixed penjaluran komponen

--- src/container/main/Route.jsx
+++ src/container/main/Route.jsx
@@ -1,4 +1,6 @@
 import React, { Component, Fragment } from "react";
+import "./Route.css"
+import logo from '../../logo.svg';
 import {
     BrowserRouter,
     Route,
@@ -9,52 +11,58 @@ import {
 import Home from "../../component/route/Home"
 import About from "../../component/route/About"
 import Pricing from "../../component/route/Pricing"
+import Post from "../post/Post"
 
-class Routed extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <Fragment>
-                    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                        <div className="container-fluid">
-                            <a className="navbar-brand" href="/">Navbar</a>
-                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                <span className="navbar-toggler-icon"></span>
-                            </button>
-                            <div className="collapse navbar-collapse" id="navbarNav">
-                                <ul className="navbar-nav">
-                                    <li className="nav-item">
-                                        <div className="nav-link">
-                                            <Link to="/">Home</Link>
-                                        </div>
-                                    </li>
-                                    <li className="nav-item">
-                                        <div className="nav-link">
-                                            <Link to="/about">About</Link>
-                                        </div>
-                                    </li>
-                                    <li className="nav-item">
-                                        <div className="nav-link">
-                                            <Link to="/pricing">Pricing</Link>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
+const Routed = () => {
+    return (
+        <BrowserRouter>
+            <Fragment>
+                <nav className="navbar navbar-expand-lg bg-dark">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="/">
+                            <img src={logo} className="App-logo" width="50px" alt="logo" />
+                        </a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <div className="nav-link">
+                                        <Link to="/" className="text-decoration-none text-info">Home</Link>
+                                    </div>
+                                </li>
+                                <li className="nav-item">
+                                    <div className="nav-link">
+                                        <Link to="/about" className="text-decoration-none text-info">About</Link>
+                                    </div>
+                                </li>
+                                <li className="nav-item">
+                                    <div className="nav-link">
+                                        <Link to="/pricing" className="text-decoration-none text-info">Pricing</Link>
+                                    </div>
+                                </li>
+                                <li className="nav-item">
+                                    <div className="nav-link">
+                                        <Link to="/post" className="text-decoration-none text-info">Post</Link>
+                                    </div>
+                                </li>
+                            </ul>
                         </div>
-                    </nav>
-                    <div className="container">
-                        <h1 className="text-center">MENGGUNAKAN ROUTE</h1>
-                        <hr />
                     </div>
+                </nav>
+                <div className="container">
+                    <h1 className="text-center">MENGGUNAKAN ROUTE</h1>
+                    <hr />
                     <Routes>
-                        <Route path="/" component={<Home />} />
-                        <Route path="/about" component={<About />} />
-                        <Route path="/pricing" component={<Pricing />} />
+                        <Route path="/" element={<Home />}></Route>
+                        <Route path="/about" element={<About />} ></Route>
+                        <Route path="/pricing" element={<Pricing />} ></Route>
+                        <Route path="/post" element={<Post />} ></Route>
                     </Routes>
-                </Fragment >
-            </BrowserRouter>
-        )
-
-    }
+                </div>
+            </Fragment >
+        </BrowserRouter>
+    )
 }
-export default Routed;
+export default Routed
