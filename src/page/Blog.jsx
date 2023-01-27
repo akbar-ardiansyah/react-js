5324667bf85163995284964e1a4d7a3e45073f78
akbar-ardiansyah <73751746+akbar-ardiansyah@users.noreply.github.com>
2023-01-28 00:18:30 +0700

    menangkap props dari param yg dikirimkan

--- src/page/Blog.jsx
+++ src/page/Blog.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import React, { Component, Fragment } from "react";
 // component
 import BlockPost from "../component/blockpost/Blockpost";
+import Ads from "../component/Ads/Ads"
 
 class Blog extends Component {
     state = {
@@ -10,7 +11,7 @@ class Blog extends Component {
     }
     getPostApi = () => {
         axios.get('http://localhost:3004/posts?_sort=id&_order=desc').then((result) => {
-            console.log(result.data);
+            // console.log(result.data);
             this.setState({
                 blogPost: result.data
             })
@@ -22,21 +23,35 @@ class Blog extends Component {
     componentDidMount() {
         this.getPostApi()
     }
+    handleDetailPost = (id) => {
+        console.log(id)
+    }
+
     render() {
         return (
             <Fragment>
                 <section>
                     <div className="container">
                         <h1>BlogPost</h1>
-                        <div className="col-10">
-                            {
-                                this.state.blogPost.map(blogPost => {
-                                    return (
-                                        // <BlockPost key={blogPost.id.toString()} title={blogPost.title} excerpt={blogPost.body} authors={blogPost.authors} />
-                                        <BlockPost key={blogPost.id} title={blogPost.title} excerpt={blogPost.body.length < 200 ? blogPost.body.substring(0, 200) : blogPost.body.substring(0, 200) + " ..."} authors="Rahmad" />
-                                    )
-                                })
-                            }
+                        <div className="row">
+                            <div className="col-md-10 col-sm-12 mx-auto">
+                                {
+                                    this.state.blogPost.map(blogPost => {
+                                        return (
+                                            <BlockPost key={blogPost.id}
+                                                title={blogPost.title}
+                                                excerpt={blogPost.body.length < 200 ? blogPost.body.substring(0, 200) : blogPost.body.substring(0, 200) + " ..."}
+                                                authors="Rahmad"
+                                                detailPost={this.handleDetailPost} />
+                                        )
+                                    })
+                                }
+                            </div>
+                            <div className="col-md-2 mx-auto">
+                                <div className="mt-5">
+                                    <Ads />
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </section>
