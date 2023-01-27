81fb8d68dfce26d6d4c50ab7e12d7b5b68634b89
akbar-ardiansyah <73751746+akbar-ardiansyah@users.noreply.github.com>
2023-01-28 00:15:54 +0700

    menambah komponent  ads

---
+++ src/component/Ads/Ads.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../Ads/Ads.css"
+const Ads = () => {
+    return (
+        <div>
+            <div className="card p-3 bg-grape">
+                <div className="card-title h5">
+                    Anda Mencari Kerja?
+                </div>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, repellendus?
+            </div>
+        </div>
+    )
+}
+export default Ads;
